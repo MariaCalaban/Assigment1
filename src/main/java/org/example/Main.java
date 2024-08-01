c19427bb0b8db26a253cec2cf6e38a63e0c5df93
Maria <maria.calaban@ext.evozon.com>
2024-08-01 16:55:44 +0300

    vvv

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -47,8 +47,6 @@ public class Main {
         System.out.println("Vremea este: " + op.vremebuna(5));
 
         op.countToHundred(50);
-        op.countToHundred(50);
-        op.countToHundred(50);
     }
 
 }
